7ccd8dc9d983ef97be9031735d9175f7564bc916
Neim4ch <64603880+Neim4ch@users.noreply.github.com>
2022-04-17 03:52:33 +0700

    added new member

--- Source.cpp
+++ Source.cpp
@@ -1,114 +1,111 @@
-#include "Source.h"
-using namespace std;
-void feature::InData(ifstream& ifst) {
-	ifst >> director;
-}
-void feature::Out(ofstream& ofst) {
-	ofst << "It is feature film. Director is " << director << endl;
-}
-void animation::InData(ifstream& ifst) {
-	int t;
-	ifst >> t;
++ACM-include +ACI-Source.h+ACI-
+using namespace std+ADs-
+void feature::InData(ifstream+ACY- ifst) +AHs-
+	ifst +AD4APg- director+ADs-
+	ifst +AD4APg- country+ADs-
++AH0-
+void feature::Out(ofstream+ACY- ofst) +AHs-
+	ofst +ADwAPA- +ACI-It is feature film. Director is +ACI- +ADwAPA- director +ADwAPA- endl+ADs-
+	ofst +ADwAPA- +ACI-This picture was filmed in +ACI- +ADwAPA- country +ADwAPA- endl+ADs-
++AH0-
+void animation::InData(ifstream+ACY- ifst) +AHs-
+	int t+ADs-
+	ifst +AD4APg- t+ADs-
 	switch (t)
-	{
+	+AHs-
 	case 1:
-		woc = DRAWN;
-		break;
+		woc +AD0- DRAWN+ADs-
+		break+ADs-
 	case 2:
-		woc = DOLL;
-		break;
+		woc +AD0- DOLL+ADs-
+		break+ADs-
 	case 3:
-		woc = STOP_MOTION;
-		break;
-	}
-}
-void animation::Out(ofstream& ofst) {
+		woc +AD0- STOP+AF8-MOTION+ADs-
+		break+ADs-
+	+AH0-
+	ifst +AD4APg- country+ADs-
++AH0-
+void animation::Out(ofstream+ACY- ofst) +AHs-
 	switch (woc)
-	{
+	+AHs-
 	case 0:
-		//woc = DRAWN;
-		ofst << "It is animation film. It's way of creation is drawing." << endl;
-		break;
+		ofst +ADwAPA- +ACI-It is animation film. It's way of creation is drawing.+ACI- +ADwAPA- endl+ADs-
+		break+ADs-
 	case 1:
-		//woc = DOLL;
-		ofst << "It is animation film. It's way of creation is using dolls" << endl;
-		break;
+		ofst +ADwAPA- +ACI-It is animation film. It's way of creation is using dolls+ACI- +ADwAPA- endl+ADs-
+		break+ADs-
 	case 2:
-		//woc = STOP_MOTION;
-		ofst << "It is animation film. It's way of creation is stop motion" << endl;
-		break;
-	}
-
-	//ofst << "It is animation film. It's way of creation is " << woc << endl;
-}
-film* film::In(ifstream& ifst) {
-	film* fl;
-	int k;
-	ifst >> k;
-	switch (k) {
+		ofst +ADwAPA- +ACI-It is animation film. It's way of creation is stop motion+ACI- +ADwAPA- endl+ADs-
+		break+ADs-
+	+AH0-
+	ofst +ADwAPA- +ACI-This picture was filmed in +ACI- +ADwAPA- country +ADwAPA- endl+ADs-
++AH0-
+film+ACo- film::In(ifstream+ACY- ifst) +AHs-
+	film+ACo- fl+ADs-
+	int k+ADs-
+	ifst +AD4APg- k+ADs-
+	switch (k) +AHs-
 	case 1:
-		fl = new feature;
-		break;
+		fl +AD0- new feature+ADs-
+		break+ADs-
 	case 2:
-		fl = new animation;
-		break;
+		fl +AD0- new animation+ADs-
+		break+ADs-
 	default:
-		return 0;
-	}
-	fl->InData(ifst);
-	return fl;
-}
-Node::Node(film* newpic)
-{
-	pic = newpic;
-	next = NULL;
-}
+		return 0+ADs-
+	+AH0-
+	fl-+AD4-InData(ifst)+ADs-
+	return fl+ADs-
++AH0-
+Node::Node(film+ACo- newpic)
++AHs-
+	pic +AD0- newpic+ADs-
+	next +AD0- NULL+ADs-
++AH0-
 container::container()
-{
-	head = NULL;
-	head = NULL;
-	size = 0;
-}
-void container::Clear() {
-	head = NULL;
-	curr = NULL;
-	size = 0;
-}
-void container::In(ifstream& ifst) {
-	while (!ifst.eof()) {
-		Node* newNode;
-		newNode = new Node((film::In(ifst)));
-		if (head == NULL)
-		{
-			head = newNode;
-			size = 1;
-		}
++AHs-
+	head +AD0- NULL+ADs-
+	head +AD0- NULL+ADs-
+	size +AD0- 0+ADs-
++AH0-
+// +AM4A9wDoAPEA8gDqAOA- +AOoA7gDtAPIA5QDpAO0A5QDwAOA- +AO4A8g- +AP0A6wDlAOwA5QDtAPIA7gDi-
+void container::Clear() +AHs-
+	head +AD0- NULL+ADs-
+	curr +AD0- NULL+ADs-
+	size +AD0- 0+ADs-
++AH0-
+
+void container::In(ifstream+ACY- ifst) +AHs-
+	while (+ACE-ifst.eof()) +AHs-
+		Node+ACo- newNode+ADs-
+		newNode +AD0- new Node((film::In(ifst)))+ADs-
+		if (head +AD0APQ- NULL)
+		+AHs-
+			head +AD0- newNode+ADs-
+			size +AD0- 1+ADs-
+		+AH0-
 		else
-		{
-			curr = head;
-			while (curr->next != NULL)
-			{
-				curr = curr->next;
-			}
-			curr->next = newNode;
-			size++;
-		}
-	}
-}
-void container::Out(ofstream& ofst) {
-	ofst << "Container contents " << size
-		<< " elements." << endl;
-	/*for (int i = 0; i < size; i++) {
-		ofst << i << ": ";
-		cont[i]->Out(ofst);
-	}*/
-	int i = 0;
-	curr = head;
-	while (curr != NULL)
-	{
-		ofst << i << ": ";
-		curr->pic->Out(ofst);
-		curr = curr->next;
-		i++;
-	}
-}
+		+AHs-
+			curr +AD0- head+ADs-
+			while (curr-+AD4-next +ACEAPQ- NULL)
+			+AHs-
+				curr +AD0- curr-+AD4-next+ADs-
+			+AH0-
+			curr-+AD4-next +AD0- newNode+ADs-
+			size+-+-+ADs-
+		+AH0-
+	+AH0-
++AH0-
+void container::Out(ofstream+ACY- ofst) +AHs-
+	ofst +ADwAPA- +ACI-Container contents +ACI- +ADwAPA- size
+		+ADwAPA- +ACI- elements.+ACI- +ADwAPA- endl+ADs-
+	int i +AD0- 0+ADs-
+	curr +AD0- head+ADs-
+	while (curr +ACEAPQ- NULL)
+	+AHs-
+		ofst +ADwAPA- i +ADwAPA- +ACI-: +ACIAOw-
+		curr-+AD4-pic-+AD4-Out(ofst)+ADs-
+		curr +AD0- curr-+AD4-next+ADs-
+		i+-+-+ADs-
+	+AH0-
++AH0-
--- Source.h
+++ Source.h
@@ -1,12 +1,14 @@
-#pragma once
+﻿#pragma once
 #include <iostream>
 #include <fstream>
 using namespace std;
 class film {
 public:
+	string country;
+	// èäåíèòôèêàöèÿ, ïîðîæäåíèå è ââîä ôèãóðû èç ïîòîêà
 	static film* In(ifstream& ifst);
-	virtual void InData(ifstream& ifst) = 0;
-	virtual void Out(ofstream& ofst) = 0;
+	virtual void InData(ifstream& ifst) = 0; // ââîä
+	virtual void Out(ofstream& ofst) = 0; // âûâîä
 };
 class Node {
 public:
@@ -15,31 +17,35 @@ public:
 	Node(film* newfigure);
 	//~Node() {}
 };
+// èãðîâîé ôèëüì
 class feature : public film {
-	string director;
+	string director; // ðåæèñåð
+
 public:
-	void InData(ifstream& ifst);
-	void Out(ofstream& ofst);
-	feature() {}
+	// ïåðåîïðåäåëÿåì èíòåðôåéñ êëàññà
+	void InData(ifstream& ifst); // ââîä
+	void Out(ofstream& ofst); // âûâîä
+	feature() {} // ñîçäàíèå áåç èíèöèàëèçàöèè.
 };
+// ìóëüòôèëüì
 class animation : public film {
-	enum way { DRAWN, DOLL, STOP_MOTION };
-	way woc;
+	enum way { DRAWN, DOLL, STOP_MOTION };// ñïîñîá ñîçäàíèÿ
+	way woc;//ñïîñîá ñîçäàíèÿ
 public:
-	void InData(ifstream& ifst);
-	void Out(ofstream& ofst);
-	animation() {}
+	// ïåðåîïðåäåëÿåì èíòåðôåéñ êëàññà
+	void InData(ifstream& ifst); // ââîä
+	void Out(ofstream& ofst); // âûâîä
+	animation() {} // ñîçäàíèå áåç èíèöèàëèçàöèè.
 };
 class container {
-
 public:
 	Node* head;
 	Node* curr;
 	int size;
 
-	void In(ifstream& ifst);
-	void Out(ofstream& ofst);
-	void Clear();
-	container();
-	~container() { Clear(); }
+	void In(ifstream& ifst); // ââîä
+	void Out(ofstream& ofst); // âûâîä
+	void Clear(); // î÷èñòêà êîíòåéíåðà îò ôèãóð
+	container(); // èíèöèàëèçàöèÿ êîíòåéíåðà
+	~container() { Clear(); } // óòèëèçàöèÿ êîíòåéíåðà
 };
